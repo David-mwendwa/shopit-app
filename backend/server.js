6aa452a62ec113d0562cb9cecc4c4ddbbc4c570e
techdave <klatzdavie19@gmail.com>
2022-04-19 17:01:33 +0300

    do initial setup and connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 require('dotenv').config({ path: 'config/config.env' });
 const app = require('./app');
 
+// database
+const connectDB = require('./db/connect');
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
+
+// connection
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`server listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
