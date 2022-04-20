08062b69bdd3159fb2e2ad01703f0628a9c79356
techdave <klatzdavie19@gmail.com>
2022-04-20 17:44:57 +0300

    add products seeder utility

---
+++ backend/utils/seeder.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: '../config/config.env' });
+const Product = require('../models/Product');
+const products = require('../data/products.json');
+const connectDB = require('../db/connect');
+
+const seedProducts = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.insertMany(products);
+    console.log('products added successfully');
+    process.exit(0);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+seedProducts();
