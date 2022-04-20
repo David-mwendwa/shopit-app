dc0afde3d5d29decc122c00349fa7f20f0098115
techdave <klatzdavie19@gmail.com>
2022-04-20 16:41:59 +0300

    create product model

---
+++ backend/models/Product.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter product name'],
+    trim: true,
+    maxlength: [100, 'product name cannot execeed 100 characters'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please enter product price'],
+    maxlength: [5, 'product price cannot execeed 5 characters'],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please enter product description'],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, 'Please select category for this product'],
+    enum: {
+      values: [
+        'Electronics',
+        'Cameras',
+        'Laptop',
+        'Accessories',
+        'Headphone',
+        'Food',
+        'Books',
+        'Clothes/Shoes',
+        'Sports',
+        'Outdoor',
+        'Home',
+      ],
+      message: 'Please select correct category for the product',
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, 'Please select product seller'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Please select product stock'],
+    maxlength: [5, 'Product name cannot execeed 5 characters'],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ const app = require('./app');
 // database
 const connectDB = require('./db/connect');
 
-const port = process.env.PORT || 5000;
-
 // connection
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
