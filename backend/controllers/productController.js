5c11eac32283865c2346a57fbe4e1af5b74df6bd
techdave <klatzdavie19@gmail.com>
2022-04-20 18:04:46 +0300

    add a route to get product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,10 +10,22 @@ exports.newProduct = async (req, res, next) => {
 };
 
 exports.getProducts = async (req, res, next) => {
-  const products = await Product.find({})
+  const products = await Product.find({});
   res.status(StatusCodes.OK).json({
     success: true,
     count: products.length,
     message: products,
   });
+};
+
+exports.getSingleProduct = async (req, res, next) => {
+  const { id: productId } = req.params;
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      success: false,
+      message: 'product not found',
+    });
+  }
+  res.status(StatusCodes.OK).json({ success: true, product });
 };
