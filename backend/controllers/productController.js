b25fe937f526f51c313bc28218bb6ff32e5d0952
techdave <klatzdavie19@gmail.com>
2022-04-20 17:13:02 +0300

    add product creation route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,13 @@
 const { StatusCodes } = require('http-status-codes');
+const Product = require('../models/Product');
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    product,
+  });
+};
 
 exports.getProducts = (req, res, next) => {
   res.status(StatusCodes.OK).json({
