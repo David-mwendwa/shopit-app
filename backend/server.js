740de34b855794967b503880583fc9414239444a
techdave <klatzdavie19@gmail.com>
2022-04-21 11:07:04 +0300

    handle unhandled promise rejection

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,28 @@ const app = require('./app');
 const connectDB = require('./db/connect');
 
 // connection
+connectDB(process.env.MONGO_URI);
 const port = process.env.PORT || 5000;
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => console.log(`server listening on port ${port}`));
-  } catch (error) {
-    console.log(error);
-  }
-};
-start();
+const server = app.listen(port, () =>
+  console.log(`server listening on port ${port}`)
+);
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// // connection
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI);
+//     app.listen(port, () => console.log(`server listening on port ${port}`));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// start();
