3b521931b8218ddc136fa19111bf327c5ebdaba0
techdave <klatzdavie19@gmail.com>
2022-04-22 10:41:40 +0300

    create APIFeatures class add add query search functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,7 @@ const Product = require('../models/Product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -13,7 +14,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find({});
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+  //console.log('apifeatures', apiFeatures);
+  const products = await apiFeatures.query;
   res.status(StatusCodes.OK).json({
     success: true,
     count: products.length,
