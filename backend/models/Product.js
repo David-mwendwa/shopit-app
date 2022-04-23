acdb31cbe91cfcb8d6af288ad4cedd8f10305eaf
techdave <klatzdavie19@gmail.com>
2022-04-23 18:02:19 +0300

    add order model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -84,7 +84,7 @@ const productSchema = new mongoose.Schema({
     },
   ],
   user: {
-    type: mongoose.Schema.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
