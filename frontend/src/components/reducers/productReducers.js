97677878dc4a6af7251f50b2f69cba4150b1fae3
techdave <klatzdavie19@gmail.com>
2022-04-25 11:24:19 +0300

    add all products reducer

---
+++ frontend/src/components/reducers/productReducers.js
@@ -0,0 +1,36 @@
+import {
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_FAIL,
+  CLEAR_ERRORS,
+} from '../../constants/productConstants';
+
+const initialState = { products: [] };
+
+export const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ALL_PRODUCTS_REQUEST:
+      return {
+        loading: true,
+        products: [],
+      };
+    case ALL_PRODUCTS_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.productsCount,
+      };
+    case ALL_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productReducer } from './components/reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  products: productReducer,
+});
 
 let initialState = {};
 
