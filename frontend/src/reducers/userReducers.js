9108fe70d196b01489cd5150e4daca039d9e2b8b
techdave <klatzdavie19@gmail.com>
2022-04-26 11:44:53 +0300

    create register user Reducer and Action

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,17 +2,22 @@ import {
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
   CLEAR_ERRORS,
 } from '../constants/userConstants';
 
 export const authReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
       return {
         loading: true,
         isAuthenticated: false,
       };
     case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -20,6 +25,7 @@ export const authReducer = (state = { user: {} }, action) => {
         user: action.payload,
       };
     case LOGIN_FAIL:
+    case REGISTER_USER_FAIL:
       return {
         ...state,
         loading: false,
