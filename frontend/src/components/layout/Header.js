e6378506988ee1bc914ff266ca3ac827e515b4d4
techdave <klatzdavie19@gmail.com>
2022-04-27 07:12:35 +0300

    show user in the Header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -9,7 +9,7 @@ import { logout } from '../../actions/userActions';
 const Header = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
-  //const { user, loading } = useSelector((state) => state.auth);
+  const { user, loading } = useSelector((state) => state.auth);
 
   const logoutHandler = () => {
     dispatch(logout());
@@ -33,16 +33,59 @@ const Header = () => {
         </div>
 
         <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-          <Link to='/login' className='btn ml-4' id='login_btn'>
-            Login
+          <Link to='/cart' style={{ textDecoration: 'none' }}>
+            <span id='cart' className='ml-3'>
+              Cart
+            </span>
+            <span className='ml-1' id='cart_count'>
+              2
+            </span>
           </Link>
-
-          <span id='cart' className='ml-3'>
-            Cart
-          </span>
-          <span className='ml-1' id='cart_count'>
-            2
-          </span>
+          {user ? (
+            <div className='ml-4 dropdown d-inline'>
+              <Link
+                to='#'
+                className='btn dropdown-toggle text-white'
+                type='button'
+                id='dropDownMenuButton'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'>
+                <figure className='avatar avatar-nav'>
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    className='rounded-cirlce'
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </Link>
+              <div className='dropdown-menu' aria-label='dropDownMenuButton'>
+                \
+                {user && user.role !== 'admin' ? (
+                  <Link className='dropdown-item' to='/orders/me'>
+                    Orders
+                  </Link>
+                ) : (
+                  <Link className='dropdown-item' to='/dashboard'>
+                    Dashboard
+                  </Link>
+                )}
+                <Link className='dropdown-item' to='/me'>
+                  Profile
+                </Link>
+                <Link className='dropdown-item text-danger' to='/'>
+                  Logout
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link to='/login' className='btn ml-4' id='login_btn'>
+                Login
+              </Link>
+            )
+          )}
         </div>
       </nav>
     </Fragment>
