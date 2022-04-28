ec36a1618f0f825a14d7e67288cb69962719ae46
techdave <klatzdavie19@gmail.com>
2022-04-28 17:43:32 +0300

    handle quantity selection

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 import Loader from '../layout/Loader';
 import MetaData from '../layout/MetaData';
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getProductDetails, clearErrors } from '../../actions/productActions';
 
 const ProductDetails = () => {
+  const [quantity, setQuantity] = useState(1);
   const alert = useAlert();
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -24,6 +25,20 @@ const ProductDetails = () => {
       dispatch(clearErrors());
     }
   }, [dispatch, alert, error, id]);
+
+  const increaseQty = () => {
+    const count = document.querySelector('.count');
+    if (count.valueAsNumber >= product.stock) return;
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQty = () => {
+    const count = document.querySelector('.count');
+    if (count.valueAsNumber <= 1) return;
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty);
+  };
 
   return (
     <Fragment>
@@ -65,18 +80,22 @@ const ProductDetails = () => {
 
               <hr />
 
-              <p id='product_price'>$108.00</p>
+              <p id='product_price'>${product.price}</p>
               <div className='stockCounter d-inline'>
-                <span className='btn btn-danger minus'>-</span>
+                <span className='btn btn-danger minus' onClick={decreaseQty}>
+                  -
+                </span>
 
                 <input
                   type='number'
                   className='form-control count d-inline'
-                  value='1'
+                  value={quantity}
                   readonly
                 />
 
-                <span className='btn btn-primary plus'>+</span>
+                <span className='btn btn-primary plus' onClick={increaseQty}>
+                  +
+                </span>
               </div>
               <button
                 type='button'
