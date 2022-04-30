5f8b50be949c83bb33bbd17f5e96ce4999d4032c
techdave <klatzdavie19@gmail.com>
2022-04-30 13:59:45 +0300

    crate new order Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,7 @@ import {
   userReducer,
 } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { newOrderReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
   products: productReducer,
@@ -19,6 +20,7 @@ const reducer = combineReducers({
   user: userReducer,
   forgotPassword: forgotPasswordReducer,
   cart: cartReducer,
+  newOrder: newOrderReducer
 });
 
 let initialState = {
