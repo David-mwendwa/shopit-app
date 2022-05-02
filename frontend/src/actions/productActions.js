1e1bfb3c204788c633663d39d7f2196f4f830437
techdave <klatzdavie19@gmail.com>
2022-05-02 11:35:15 +0300

    add _update Product_ Reducer and Action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -13,6 +13,10 @@ import {
   DELETE_PRODUCT_REQUEST,
   DELETE_PRODUCT_SUCCESS,
   DELETE_PRODUCT_RESET,
+  UPDATE_PRODUCT_REQUEST,
+  UPDATE_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_RESET,
+  UPDATE_PRODUCT_FAIL,
   DELETE_PRODUCT_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
@@ -76,6 +80,29 @@ export const deleteProduct = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: DELETE_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Update product (ADMIN)
+export const updateProduct = (id, productData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PRODUCT_REQUEST });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.patch(
+      `/api/v1/admin/product/${id}`,
+      productData,
+      config
+    );
+    dispatch({ type: UPDATE_PRODUCT_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PRODUCT_FAIL,
       payload: error.response.data.message,
     });
   }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -13,6 +13,10 @@ import {
   DELETE_PRODUCT_SUCCESS,
   DELETE_PRODUCT_RESET,
   DELETE_PRODUCT_FAIL,
+  UPDATE_PRODUCT_REQUEST,
+  UPDATE_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_RESET,
+  UPDATE_PRODUCT_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
@@ -63,6 +67,7 @@ export const productsReducer = (state = { products: [] }, action) => {
 export const productReducer = (state = {}, action) => {
   switch (action.type) {
     case DELETE_PRODUCT_REQUEST:
+    case UPDATE_PRODUCT_REQUEST:
       return {
         ...state,
         loading: true,
@@ -74,7 +79,15 @@ export const productReducer = (state = {}, action) => {
         isDeleted: action.payload,
       };
     }
+    case UPDATE_PRODUCT_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+    }
     case DELETE_PRODUCT_FAIL:
+    case UPDATE_PRODUCT_FAIL:
       return {
         loading: false,
         error: action.payload,
@@ -82,7 +95,12 @@ export const productReducer = (state = {}, action) => {
     case DELETE_PRODUCT_RESET:
       return {
         ...state,
-        success: false,
+        isDeleted: false,
+      };
+    case UPDATE_PRODUCT_RESET:
+      return {
+        ...state,
+        isUpdated: false,
       };
     case CLEAR_ERRORS:
       return {
